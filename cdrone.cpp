25aa80a70181619dfc64a40aba9c5aafbeb36c9d
Freddie Rice <freddie.rice@gmail.com>
2017-11-02 23:22:39 -0400

    testing multiwii from cdrone

--- cdrone.cpp
+++ cdrone.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
 
 #include "config.h"
 #include "watchdog.h"
+#include "serial.h"
+#include "MultiWii.h"
 
 int main(int argc, const char *argv[]) try {
+
+	/*
+	Serial serial("/dev/ttyUSB0");
+	MultiWii m(serial);
+	m.sendCMD(MultiWiiCMD::MSP_STATUS);
+	for (;;) {
+		char b;
+		if (serial.readFull(&b, 1)) {
+			std::cout << b;
+			std::cout.flush();
+		}
+	}
+
+	return 0;
+	*/
+
 	Config config("cdrone.conf");
 	Watchdog watchdog(3,0);
 
